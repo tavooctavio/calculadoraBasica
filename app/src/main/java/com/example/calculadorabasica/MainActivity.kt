3667c794d62d8c7ef4b55a82a97a8f065b1f1b6e
Octavio Resendiz Bolaños <octavioresendiz36@aragon.unam.mx>
2022-03-19 20:59:51 -0600

    El nombre de usuario que se pide al inicio se pasa a la seguda y pricipal activity

--- app/src/main/java/com/example/calculadorabasica/MainActivity.kt
+++ app/src/main/java/com/example/calculadorabasica/MainActivity.kt
@@ -2,10 +2,21 @@ package com.example.calculadorabasica
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var tvUsuario = findViewById<TextView>(R.id.tvUsuario)
+
+
+        var usuario: String? = intent.getStringExtra("usuario")
+        tvUsuario.text = usuario
+
+
+
     }
 }
--- app/src/main/java/com/example/calculadorabasica/PantallaInicio.kt
+++ app/src/main/java/com/example/calculadorabasica/PantallaInicio.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.EditText
 
 class PantallaInicio : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,9 +14,11 @@ class PantallaInicio : AppCompatActivity() {
 
 
         val btIniciar = findViewById<Button>(R.id.btListo)
+        val etNombreUsuario = findViewById<EditText>(R.id.etNombreUsuario)
 
         btIniciar.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
+            intent.putExtra("usuario",etNombreUsuario.text.toString())
             startActivity(intent)
         }
 
